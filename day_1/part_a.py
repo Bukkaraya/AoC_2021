ebcd74dea6fcdfadf0af2cb0ce8e7fba14cb7256
Abinav Bukkaraya <11041625+Bukkaraya@users.noreply.github.com>
2021-12-02 00:07:27 +0000

    Added day 1 of AoC

---
+++ day_1/part_a.py
@@ -0,0 +1,17 @@
+def count_num_increases(measurements):
+	num_increases = 0
+	for i in range(len(measurements) - 1):
+		if measurements[i + 1] > measurements[i]:
+			num_increases += 1
+	
+	return num_increases
+
+
+if __name__ == "__main__":
+	with open("input_a.txt", "r") as f:
+		measurements = [int(i) for i in f.read().split("\n")]
+
+	print(f"Part A: Number of increases: {count_num_increases(measurements)}")
+
+	filtered_measurements = [sum(measurements[i:i+3]) for i in range(len(measurements)- 2)]
+	print(f"Part B: Number of increases: {count_num_increases(filtered_measurements)}")
